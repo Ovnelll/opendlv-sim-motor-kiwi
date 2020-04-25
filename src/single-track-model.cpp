de6f84bacf367e184ad3c2e6b30066a0ddf1d81c
Ovnelll <58163501+Ovnelll@users.noreply.github.com>
2020-04-25 14:00:58 +0200

    Update single-track-model.cpp

--- src/single-track-model.cpp
+++ src/single-track-model.cpp
@@ -46,14 +46,6 @@ void SingleTrackModel::setPedalPosition(opendlv::proxy::PedalPositionRequest con
 opendlv::sim::KinematicState SingleTrackModel::step(double dt) noexcept
 {
   double const pedalSpeedGain{0.5};
-
-  double const mass{1.0};
-  double const momentOfInertiaZ{0.1};
-  double const length{0.22};
-  double const frontToCog{0.11};
-  double const rearToCog{length - frontToCog};
-  double const corneringStiffnessFront{1.0};
-  double const corneringStiffnessRear{1.0};
   
   float groundSteeringAngleCopy;
   float pedalPositionCopy;
@@ -65,28 +57,8 @@ opendlv::sim::KinematicState SingleTrackModel::step(double dt) noexcept
   }
 
   m_longitudinalSpeed = pedalPositionCopy * pedalSpeedGain;
-
-  if (std::abs(m_longitudinalSpeed) > 0.01f) {
-    double const slipAngleFront = groundSteeringAngleCopy 
-      - (m_lateralSpeed + frontToCog * m_yawRate) 
-      / std::abs(m_longitudinalSpeed);
-    double const slipAngleRear = (rearToCog * m_yawRate - m_lateralSpeed) 
-      / std::abs(m_longitudinalSpeed);
-
-    double const lateralSpeedDot = (corneringStiffnessFront * slipAngleFront 
-        + corneringStiffnessRear * slipAngleRear)
-      / (mass - m_longitudinalSpeed * m_yawRate);
-
-    double const yawRateDot = (frontToCog * corneringStiffnessFront * slipAngleFront 
-        - rearToCog * corneringStiffnessRear * slipAngleRear)
-      / momentOfInertiaZ;
-    
-    m_lateralSpeed += lateralSpeedDot * dt;
-    m_yawRate += yawRateDot * dt;
-  } else {
-    m_lateralSpeed = 0.0f;
-    m_yawRate = 0.0f;
-  }
+  m_lateralSpeed = 0.0f;
+  m_yawRate = groundSteeringAngleCopy;
 
   opendlv::sim::KinematicState kinematicState;
   kinematicState.vx(static_cast<float>(m_longitudinalSpeed));
